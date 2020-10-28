915ba6010e3619aa014de4d7cd8d52c9837c07b8
Aniket Kumar <aniketkumar049@gmail.com>
2020-10-28 12:37:23 +0530

    add global application wrapper component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import { addDecorator } from "@storybook/react";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../components/particles/globalStyles";
+import themeDefault from "../components/particles/themeDefault";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+const GlobalWrapper = (storyFn) => (
+  <ThemeProvider theme={themeDefault}>
+    <GlobalStyles />
+    {storyFn()}
+  </ThemeProvider>
+);
+
+addDecorator(GlobalWrapper);
