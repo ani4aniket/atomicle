59ceff7472c21cb1f1fd49a335fe84d46f303be2
Aniket Kumar <aniketkumar049@gmail.com>
2020-10-28 14:49:35 +0530

    add svg support

--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Button from "./button";
 
-export const basicButton = () => <Button>Basic button</Button>;
-
 const buttonClicked = (e) => {
   e.preventDefault();
-  alert("hello");
+  alert("Hello");
 };
 
-export const functionButton = () => (
-  <Button onClick={buttonClicked} variant="secondary">
-    Function button
-  </Button>
+export const basicButton = () => <Button>Basic button</Button>;
+export const secondaryButton = () => (
+  <Button variant="secondary">Secondary button</Button>
 );
-
+export const tertiaryButton = () => (
+  <Button variant="tertiary">Tertiary button</Button>
+);
+export const iconButton = () => <Button icon="user">Icon button</Button>;
+export const functionButton = () => (
+  <Button onClick={buttonClicked}>Function button</Button>
+);
 export const linkedButton = () => <Button href="/route">Linked button</Button>;
 
 export default {
