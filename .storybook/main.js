9b427d680d1caafcb809887dd3cdf2c31b25c3c8
Aniket Kumar <aniketkumar049@gmail.com>
2020-10-29 14:39:37 +0530

    add knobs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs",
+    "storybook-addon-designs",
+  ],
 };
--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -1,25 +1,58 @@
+import { select, text, withKnobs } from "@storybook/addon-knobs";
 import React from "react";
+import { withDesign } from "storybook-addon-designs";
 import Button from "./button";
+import knobData from "./button.knobs.json";
+
+const { icon, innerText } = knobData;
 
 const buttonClicked = (e) => {
   e.preventDefault();
   alert("Hello");
 };
 
-export const basicButton = () => <Button>Basic button</Button>;
+export const basicButton = () => (
+  <Button>{text(innerText.label, innerText.default, innerText.group)}</Button>
+);
 export const secondaryButton = () => (
-  <Button variant="secondary">Secondary button</Button>
+  <Button variant="secondary">
+    {text(innerText.label, innerText.default, innerText.group)}
+  </Button>
 );
 export const tertiaryButton = () => (
-  <Button variant="tertiary">Tertiary button</Button>
+  <Button variant="tertiary">
+    {text(innerText.label, innerText.default, innerText.group)}
+  </Button>
 );
-export const iconButton = () => <Button icon="user">Icon button</Button>;
+export const iconButton = () => (
+  <Button icon={select(icon.label, icon.options, icon.default, icon.group)}>
+    {text(innerText.label, innerText.default, innerText.group)}
+  </Button>
+);
 export const functionButton = () => (
-  <Button onClick={buttonClicked}>Function button</Button>
+  <Button onClick={buttonClicked}>
+    {text(innerText.label, innerText.default, innerText.group)}
+  </Button>
 );
-export const linkedButton = () => <Button href="/route">Linked button</Button>;
+export const linkedButton = () => (
+  <Button href="/route">
+    {text(innerText.label, innerText.default, innerText.group)}
+  </Button>
+);
+
+basicButton.story = {
+  name: "Basic Button",
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/5DExbYWfiOx8UumMD2cmAr/P-Pine-UI-Framework?node-id=12%3A71",
+    },
+  },
+};
 
 export default {
   component: Button,
   title: "Button",
+  decorators: [withKnobs, withDesign],
 };
